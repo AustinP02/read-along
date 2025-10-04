ce465d9a3196041d0fd7f570def2c2794892e4f4
Austin Pichardo <austinvladimirp@gmail.com>
2025-10-04 15:51:06 -0400

    navbar

---
+++ read-along/src/NavBar.jsx
@@ -0,0 +1,18 @@
+import './navbar.css'
+import {Link} from "react-router-dom"
+import logo from './assets/read-along-logo.png'
+
+function NavBar() {
+
+    return (
+        <nav className="nav">
+            <Link to="/"><img className='logo' src={logo} alt="Read Along logo"/></Link>
+            <ul>
+                <li><Link to="/about">About</Link></li>
+                <li><Link to="/profile">Profile</Link></li>
+            </ul>
+        </nav>
+    );
+}
+
+export default NavBar;
