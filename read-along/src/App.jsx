259c231d3fa1cbb18eb34c05036f2be80370578d
AkashSathish1 <amazingakash2020@gmail.com>
2025-10-04 13:51:39 -0400

    hello gurt

--- read-along/src/App.jsx
+++ read-along/src/App.jsx
@@ -1,21 +1,12 @@
 import { useState } from 'react'
-<<<<<<< HEAD
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-
-=======
->>>>>>> 98d84a1fb085b5ec35d6680a7fdf93cb97f455f8
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-<<<<<<< HEAD
-      
-=======
       klk
->>>>>>> 98d84a1fb085b5ec35d6680a7fdf93cb97f455f8
+      he9ifs
     </>
   )
 }
